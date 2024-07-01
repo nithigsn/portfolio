b5b7b3a79722ef86c305a8d54c0e142eef86584f
nithigsn <nithishgsn000@gmail.com>
2024-07-01 23:28:13 +0530

    added pre loading page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import Portfolio from "./Portfolio";
+import PreLoadingPage from "./Pages/PreLoadingPage";
+import { CSSTransition } from "react-transition-group";
+
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 5000);
+    };
+    fakeDataFetch();
+  }, []);
+
   return (
     <div className="App">
-      
-       <Portfolio/>
+      <CSSTransition
+        in={!isLoading}
+        timeout={500}
+        classNames="fade"
+        unmountOnExit
+      >
+        <Portfolio />
+      </CSSTransition>
+      {isLoading && <PreLoadingPage />}
     </div>
   );
 }
---
+++ src/Pages/PreLoadingPage.jsx
@@ -0,0 +1,23 @@
+export default function PreLoadingPage() {
+    return (
+
+        <div className="pre">
+
+
+            <div class="terminal-loader">
+                <div class="terminal-header">
+                    <div class="terminal-title">Status</div>
+                    <div class="terminal-controls">
+                        <div class="control close"></div>
+                        <div class="control minimize"></div>
+                        <div class="control maximize"></div>
+                    </div>
+                </div>
+                <div class="text">Loading...</div>
+            </div>
+
+
+
+        </div>
+    );
+}
